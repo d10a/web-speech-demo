6df846e28e8588339a469330b1cab2c1e6652595
DG6 <dev@davidgassama.com>
2022-07-25 14:59:08 +0200

    limit languages to fr

--- src/components/WebSpeechSynthesisTest.vue
+++ src/components/WebSpeechSynthesisTest.vue
@@ -1,41 +1,37 @@
 <script setup lang="ts">
-import { onBeforeMount, onMounted, ref, watch } from 'vue'
+import { onBeforeMount, onMounted, ref } from 'vue'
 
 
 const supportMsg = ref('')
 const speechSynthesisVoices = ref<SpeechSynthesisVoice[]>()
 const selectedVoice = ref<SpeechSynthesisVoice>()
-
+const volume = ref(0.5)
+const rate = ref(1.0)
+const pitch = ref(1.0)
 const text = ref('')
-
-const getImageUrl = (path: string) => {
-  return new URL(path, import.meta.url).href
-}
+/**
+ * Fetch voices form user's system
+ */
+onMounted(() => { })
 
 text.value = "Jeune ange aux doux regards, à la douce parole,Un instant près de vous je suis venu m'asseoir,Et, l'orage apaisé, comme l'oiseau s'envole,Mon bonheur s'en alla, n'ayant duré qu'un soir."
 
 const loadSystemVoices = () => {
   //@ts-ignore
-  speechSynthesisVoices.value = speechSynthesis.getVoices().filter(voice => {
-    return voice.lang.match(/^fr/)
+  speechSynthesisVoices.value = speechSynthesis.getVoices()
+
+  speechSynthesisVoices.value.forEach((voice: SpeechSynthesisVoice) => {
+    if (voice.default) {
+      selectedVoice.value = voice
+    }
+    return voice;
   })
-
-
 }
-
-// const setVoice = (selectedVoice) => {
-//   speechSynthesisVoices.value.forEach((voice: SpeechSynthesisVoice) => {
-//     if (voice.voiceURI === ) {
-//       selectedVoice.value = voice
-//     }
-//     return voice;
-//   })
-// }
 
 setTimeout(() => {
   loadSystemVoices()
 }, 2000)
-speechSynthesis.onvoiceschanged = loadSystemVoices();
+window.speechSynthesis.onvoiceschanged = loadSystemVoices();
 
 
 /*
@@ -60,7 +56,9 @@ const speak = () => {
 
     // Set the text.
     msg.text = text.value;
-    msg.volume = 0.5;
+    msg.volume = volume.value;
+    msg.rate = rate.value;
+    msg.pitch = pitch.value;
     msg.voice = selectedVoice.value;
     console.log(msg)
 
@@ -74,43 +72,50 @@ const speak = () => {
 
 <template>
   <div id="page-wrapper">
-    <h1>Web Speech Synthesis Demo</h1>
-    <p>(Text-to-Speech)</p>
+    <h1>Web Speech Synthesis Demo (Text-to-Speech)</h1>
 
     <p id="msg">{{ supportMsg }}</p>
 
-    <textarea type="text" name="speech-msg" id="speech-msg" x-webkit-speech v-model="text">
-    </textarea>
+    <textarea type="text" name="speech-msg" id="speech-msg" x-webkit-speech v-model="text"></textarea>
 
     <div v-if="speechSynthesisVoices.length > 0" class="option">
       <label for="voice">Voice</label>
-      <select name="voice" id="voice" v-model="selectedVoice">
-        <option v-for="voice in speechSynthesisVoices" v-bind:value="voice">
-          {{ voice.name }} - {{ voice.lang }}
+      <select name="voice" id="voice">
+        <option v-for="voice in speechSynthesisVoices" v-bind:value="voice.voiceURI">
+          {{ voice.name }}
         </option>
       </select>
     </div>
-    <button id="play-button" v-on:click="speak">
-      <img :src="getImageUrl('/assets/images/play-button.svg')" alt="Play button" />
-    </button>
+    <div class="option">
+      <label for="volume">Volume</label>
+      <input type="range" min="0" max="1" step="0.1" name="volume" id="volume" v-model="volume">
+    </div>
+    <div class="option">
+      <label for="rate">Rate</label>
+      <input type="range" min="0.1" max="10" step="0.1" name="rate" id="rate" v-model="rate">
+    </div>
+    <div class="option">
+      <label for="pitch">Pitch</label>
+      <input type="range" min="0" max="2" step="0.1" name="pitch" id="pitch" v-model="pitch">
+    </div>
+
+    <button id="speak" v-on:click="speak">Speak</button>
 
   </div>
 </template>
 
 <style scoped>
 #page-wrapper {
-  font-size: 1.6rem;
-  width: 60vw;
-  background: transparent;
-  border-radius: 10px;
-  padding: 3em;
+  width: 640px;
+  background: #FFFFFF;
+  padding: 1em;
   margin: 1em auto;
-  color: #FFFFFF;
+  border-top: 5px solid #69c773;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.8);
 }
 
 h1 {
   margin-top: 0;
-
 }
 
 #msg {
@@ -123,12 +128,26 @@ h1 {
 }
 
 textarea {
-  width: 50vw;
-  min-height: 50vh;
+  width: 80%;
   padding: 0.5em;
   font-size: 1.2em;
-  border-radius: 10px;
+  border-radius: 3px;
   border: 1px solid #D9D9D9;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1) inset;
+}
+
+input[type="range"] {
+  width: 300px;
+}
+
+label {
+  display: inline-block;
+  float: left;
+  width: 150px;
+}
+
+.option {
+  margin: 1em 0;
 }
 
 button {
@@ -136,27 +155,25 @@ button {
   border-radius: 3px;
   border: none;
   font-size: 0.9rem;
-  padding: 0.8em;
+  padding: 0.5rem 0.8em;
   background: #69c773;
+  border-bottom: 1px solid #498b50;
   color: white;
+  -webkit-font-smoothing: antialiased;
   font-weight: bold;
   margin: 0;
   width: 100%;
   text-align: center;
 }
 
-#play-button:hover,
-#play-button:focus {
+button:hover,
+button:focus {
   opacity: 0.75;
   cursor: pointer;
 }
 
-#play-button:active {
+button:active {
   opacity: 1;
   box-shadow: 0 -3px 10px rgba(0, 0, 0, 0.1) inset;
-}
-
-#play-button {
-  width: 5.5rem;
 }
 </style>
